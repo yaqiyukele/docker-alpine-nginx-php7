5b2ec28a6282bba8e81ced28a24368d86fe5a926
zhaoxiaonan <1455325631@qq.com>
2017-11-15 16:20:20 +0800

    update1

--- web/zhoubao.php
+++ web/zhoubao.php
@@ -12,7 +12,7 @@ $news = array("Title" =>"微信公众平台开发实践", "Description"=>"本书
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title>maidashuzi技术部工作周报</title>
+    <title>麦达数字技术部工作周报</title>
     <meta http-equiv="content-type" content="text/html;charset=utf-8">
     <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0,user-scalable=no" name="viewport" id="viewport" />
     <link rel="stylesheet" href="css/normalize.min.css">
@@ -40,7 +40,7 @@ $news = array("Title" =>"微信公众平台开发实践", "Description"=>"本书
             ]
         });
 
-        /*wx.checkJsApi({
+        wx.checkJsApi({
             jsApiList: [
                 'getLocation',
                 'onMenuShareTimeline',
@@ -50,7 +50,7 @@ $news = array("Title" =>"微信公众平台开发实践", "Description"=>"本书
                 alert("失败");
                 alert(JSON.stringify(res));
             }
-        });*/
+        });
 
         wx.onMenuShareAppMessage({
           title: '<?php echo $news['Title'];?>',
