0c03c1c0841ed0515730a3dc5fa41fef7a1ab942
zhaoxiaonan <1455325631@qq.com>
2017-11-15 15:32:44 +0800

    update zhoubao.php

--- web/zhoubao.php
+++ web/zhoubao.php
@@ -3,7 +3,6 @@ require "jssdk.php";
 $jssdk = new JSSDK("wx80c487097b512789", "422d3a86338493d2f7b0e56507e5ac19");//你的appid,appsecret
 $signPackage = $jssdk->GetSignPackage();
 $Getpicture = $jssdk->Getpicture();
-// print_r($Getpicture);
 // print_r($signPackage);die;
 ?>
 <!DOCTYPE html>
