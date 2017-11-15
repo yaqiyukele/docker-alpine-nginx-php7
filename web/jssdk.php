30692672608dc0bf2da60878e85123e00557a332
zhaoxiaonan <1455325631@qq.com>
2017-11-15 17:36:14 +0800

    2

--- web/jssdk.php
+++ web/jssdk.php
@@ -4,8 +4,6 @@ class JSSDK {
         private $appId;
         private $appSecret;
 
-        // session_start();
-
         public function __construct($appId, $appSecret) {
           $this->appId = $appId;
           $this->appSecret = $appSecret;
@@ -108,12 +106,5 @@ class JSSDK {
           return $res;
         }
 
-        public function Getpicture(){
-
-         $news = array("Title" =>"微信公众平台开发实践", "Description"=>"本书共分10章，案例程序采用广泛流行的PHP、MySQL、XML、CSS、JavaScript、HTML5等程序语言及数据库实现。", "PicUrl" =>'http://images.cnitblog.com/i/340216/201404/301756448922305.jpg'); 
-         return $news;
-
-        }
-
-  }
+}
 ?>
