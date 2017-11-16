2039e02f8e24368705db410d18e4963a43ad3b49
zhaoxiaonan <1455325631@qq.com>
2017-11-16 17:56:54 +0800

    1

--- web/request.php
+++ web/request.php
@@ -14,7 +14,7 @@ $aa = $test->index();
 print_r($aa);
 
 
-$mydabase=new PDO("127.0.0.1","md","maida6868","zhoubao");
+$mydabase=new PDO("172.26.249.246","md","maida6868","zhoubao");
 
 $sql = "SELECT access_token,expire_time_access_token,jsapi_ticket,expire_time_jsapi_ticket FROM cache";
 $result = $mydabase->mysql_query_rest($sql); 
