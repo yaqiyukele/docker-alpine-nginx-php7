9ad8cca51d693427b3def1cbe90193502d5863ea
zhaoxiaonan <1455325631@qq.com>
2017-11-16 15:44:49 +0800

    555

--- web/request.php
+++ web/request.php
@@ -1,8 +1,8 @@
 <?php 
-$port = 13306;         //刚才输入的本地将要使用的端口
+$port = 3306;         //刚才输入的本地将要使用的端口
 $userName = 'md';        //在服务器B上连接服务器Amysql，使用的mysql用户名
 $passwd = 'maida6868';        //改用户名对应的密码
-$con = new PDO("mysql:host=127.0.0.1:{$port}", $userName, $passwd);
+$con = new PDO("mysql:host=172.26.249.246:{$port}", $userName, $passwd);
 if($con){
 echo "success";die;
 }
