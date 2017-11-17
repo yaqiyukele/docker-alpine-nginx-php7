e1769c053fb063a7296583917e291b2bd910fb6c
zhaoxiaonan <1455325631@qq.com>
2017-11-17 13:43:51 +0800

    1

--- web/jssdk.php
+++ web/jssdk.php
@@ -20,7 +20,7 @@ class JSSDK {
           // print_r($jsapiTicket);die;
           /*$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
           $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";*/
-          $url = "http://i2137.com/php/zhoubao.php";
+          $url = "http://www.i2137.com/php/zhoubao.php?from=singlemessage";
 
           $timestamp = time();
           $nonceStr = $this->createNonceStr();
