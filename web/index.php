5e8ba8aa6bd6ca03d9ffb8dfa24674523cd5a6e3
zhaoxiaonan <1455325631@qq.com>
2017-11-24 11:42:20 +0800

    1

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once "jssdk.php";
-$jssdk = new JSSDK("wx80c487097b512789", "422d3a86338493d2f7b0e56507e5ac19",$_POST['url']);
-// $jssdk = new JSSDK("wx80c487097b512789", "422d3a86338493d2f7b0e56507e5ac19",'http://127.0.0.1/share/index.html');
+// $jssdk = new JSSDK("wx80c487097b512789", "422d3a86338493d2f7b0e56507e5ac19",$_POST['url']);
+$jssdk = new JSSDK("wx80c487097b512789", "422d3a86338493d2f7b0e56507e5ac19",'http://i2137.com/php/index.html');
 $signPackage = $jssdk->GetSignPackage();
 /*$signPackage['jsApiList'] = array('onMenuShareTimeline','onMenuShareAppMessage');
 $signPackage['debug'] = false;*/
