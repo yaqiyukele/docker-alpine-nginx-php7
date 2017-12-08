75e5c730e8730ebc193d85f29ed4d970e64d92b3
zhaoxiaonan <1455325631@qq.com>
2017-12-08 17:52:41 +0800

    2

--- web/zhoubao/ajax/htmlpost.php
+++ web/zhoubao/ajax/htmlpost.php
@@ -57,8 +57,8 @@ $result['pageC5'] = implode("@#$",$pageC5_res);
 $infoid=$_POST['q_infoid'];
 // print_r($result);die;
 
-// $mydabase=new DB("172.26.249.246","md","maida6868","zhoubao");
-$mydabase=new DB("127.0.0.1","root","root","zhoubao");
+$mydabase=new DB("172.26.249.246","md","maida6868","zhoubao");
+// $mydabase=new DB("127.0.0.1","root","root","zhoubao");
 
 $WHERE1 = "relevance_id='".$infoid."' AND page=1";
 $WHERE2 = "relevance_id='".$infoid."' AND page=2";
