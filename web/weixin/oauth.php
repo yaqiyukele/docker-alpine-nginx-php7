b7de896b2645823fcc4a06c8967c3cff60f39cf2
zhaoxiaonan <1455325631@qq.com>
2017-12-13 13:51:44 +0800

    444

--- web/weixin/oauth.php
+++ web/weixin/oauth.php
@@ -1,13 +1,12 @@
 <?php 
 include('class_weixin_adv.php');
-$appid = "wx80c487097b512789";
-$secret = "422d3a86338493d2f7b0e56507e5ac19";
-$weixin=new class_weixin_adv($appid, $secret);
+$weixin=new class_weixin_adv("wx80c487097b512789", "422d3a86338493d2f7b0e56507e5ac19");
 if (isset($_GET['code'])){
 	$url="https://api.weixin.qq.com/sns/oauth2/access_token?appid=appid&secret=secret&code=".$_GET['code']."&grant_type=authorization_code";
 	$res = $weixin->https_request($url);
 	print_r($res);
 	$res=json_decode($res, true);
+	print_r($res);
 	$row=$weixin->get_user_info($res['openid']);
 	print_r($row);die;
 	if ($row['openid']) {
