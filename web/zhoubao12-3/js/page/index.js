5dc6e60af59066127b419c7fd4f43d9bf6cec456
zhaoxiaonan <1455325631@qq.com>
2017-12-22 16:09:21 +0800

    update/12-22/12-3/2

--- web/zhoubao12-3/js/page/index.js
+++ web/zhoubao12-3/js/page/index.js
@@ -224,7 +224,7 @@ new iSlider({
                         name:'执行进度',
                         type:'bar',
                         barWidth: '30%',
-                        data:[30,20,10,30,12,0]
+                        data:[49,42,10,50,30,0]
                     }
                 ]
             };
