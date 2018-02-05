15ebf772c49663492e4ab1201bf18b5fbe0a3483
zhaoxiaonan <1455325631@qq.com>
2018-02-05 18:01:28 +0800

    update yii2.0/2-5/6

--- web/yii2/frontend/controllers/HomeController.php
+++ web/yii2/frontend/controllers/HomeController.php
@@ -41,15 +41,6 @@ class HomeController extends Controller
             $res = $this->get_to_file();
             print_r($res); 
         }
-        
-        
-<<<<<<< HEAD
-=======
-        $res = $this->get_to_file();
-        print_r($res); 
-        $authorization_code = $res[0];
->>>>>>> 399cc4188ab8302d9421f7018a7f1f96ebe0d050
-
 
         if (!empty($res[0])) {
             $client_id = "1106673362";
